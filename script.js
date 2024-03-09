c5e6b973519ee44819a6458bf0e76d31cc48f661
kakegawa <pedestrian.deck.2023@gmail.com>
2024-03-09 18:01:22 +0900

    横目盛

---
+++ script.js
@@ -0,0 +1,18 @@
+// 画面幅を取得する関数
+function getScreenWidth() {
+    return document.documentElement.clientWidth;
+}
+
+// 画面高を取得する関数
+function getScreenHeight() {
+    return document.documentElement.clientHight;
+}
+
+function displayScreenSize() {
+    
+}
+
+// 画面読込時の処理
+window.addEventListener('load', function () {
+
+});
